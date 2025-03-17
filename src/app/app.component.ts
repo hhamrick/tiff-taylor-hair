0d2ed6a78e246fba9386416d93e9349a788e0d4b
hhamrick <hhamrick@email.unc.edu>
2025-03-17 16:21:57 -0400

    Mobile sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,28 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, CommonModule, MatToolbarModule, MatButtonModule],
+  imports: [RouterOutlet, RouterLink, CommonModule, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  @ViewChild('sidenav') sidenav!: MatSidenav;
   isMobile: boolean = window.innerWidth < 600;
 
   @HostListener('window:resize', ['$event'])
   onResize() {
     this.isMobile = window.innerWidth < 600;
+    if (!this.isMobile) {
+      this.sidenav.close();
+    }
   }
 }
